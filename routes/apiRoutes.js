f70532bef78dfd6e79536a6c916d3e077c19339d
Derek Banister <ptownderek@yahoo.com>
2021-11-14 21:49:00 -0800

    all routes working on localhost, bug with heroku deployment

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,43 @@
 //json data
+// https://obscure-eyrie-11833.herokuapp.com/
+const db = require("../models");
+
+module.exports = function (app) {
+    //all workouts
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    })
+
+    app.get("/api/workouts/range", ({ }, res) => {
+        db.Workout.find({}).then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+    });
+    //create workout
+    app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+    });
+    //findby id and update one workout
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+            //id and excercises
+            { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+    });
+};
+//all routes working on localhost, heroku deploy is not working though
